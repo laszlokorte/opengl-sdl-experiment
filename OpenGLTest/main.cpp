0a9f2096ec5ee0c90fcc0b575d048d24a5596482
Laszlo Korte <me@laszlokorte.de>
2014-01-25 14:12:37 +0100

    Initial Commit

---
+++ OpenGLTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGLTest
+//
+//  Created by Laszlo Korte on 25.01.14.
+//  Copyright (c) 2014 Laszlo Korte. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
